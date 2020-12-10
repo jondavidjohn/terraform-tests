7ca7fa06558e3ab8feb7d128309f3c18bcc16fbf
Jonathan D. Johnson <me@jondavidjohn.com>
2020-12-10 17:06:49 -0600

    Update main.tf

--- random/main.tf
+++ random/main.tf
@@ -1,22 +1,15 @@
-resource "random_pet" "animal_one" {
+resource "random_pet" "animal" {
   keepers = {
     uuid = "${uuid()}" # Force a new name each time
   }
   length = 3
 }
 
-resource "random_pet" "animal_two" {
+resource "random_pet" "animal" {
   keepers = {
     uuid = "${uuid()}" # Force a new name each time
   }
   length = 2
-}
-
-resource "random_pet" "animal_three" {
-  keepers = {
-    uuid = "${uuid()}" # Force a new name each time
-  }
-  length = 4
 }
 
 output "random_one" {
@@ -25,8 +18,4 @@ output "random_one" {
 
 output "random_two" {
   value = "${random_pet.animal_two.id}"
-}
-
-output "random_three" {
-  value = "${random_pet.animal_three.id}"
 }
