f5b07c88b7f20d254c96d1beee31572ac2f5b0bc
Jonathan D. Johnson <me@jondavidjohn.com>
2022-06-06 16:08:14 -0500

    Update main.tf

--- random/main.tf
+++ random/main.tf
@@ -16,7 +16,7 @@ resource "random_pet" "animal_three" {
   keepers = {
     uuid = "${uuid()}" # Force a new name each time
   }
-  length = 7
+  length = 2
 }
 
 resource "random_pet" "animal_four" {
